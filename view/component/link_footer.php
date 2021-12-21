64721eeecb7b224b596c3f1350b1d89ce15ad1a1
Netipong <netipong42@gmail.com>
2021-12-21 15:05:36 +0700

    component

---
+++ view/component/link_footer.php
@@ -0,0 +1,64 @@
+    </div>
+    </section>
+    <!-- Main content -->
+    </div>
+    <!-- Content -->
+    <footer class="main-footer">
+        <strong>Copyright &copy; 2021 <a href="https://adminlte.io">Adminlte</a>.</strong>
+        All rights reserved.
+        <div class="float-right d-none d-sm-inline-block">
+            <b>Version</b> 3.1
+        </div>
+    </footer>
+    </div>
+    <!-- My js  -->
+    <script src="../../assets/admin_lte/dist/js/js.js"></script>
+    <!-- jQuery -->
+    <script src="../../assets/admin_lte/plugins/jquery/jquery.min.js"></script>
+    <!-- jQuery UI 1.11.4 -->
+    <script src="../../assets/admin_lte/plugins/jquery-ui/jquery-ui.min.js"></script>
+    <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
+    <script>
+        $.widget.bridge('uibutton', $.ui.button)
+    </script>
+    <!-- Bootstrap 4 -->
+    <script src="../../assets/admin_lte/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <!-- ChartJS -->
+    <script src="../../assets/admin_lte/plugins/chart.js/Chart.min.js"></script>
+    <!-- Sparkline -->
+    <script src="../../assets/admin_lte/plugins/sparklines/sparkline.js"></script>
+    <!-- JQVMap -->
+    <script src="../../assets/admin_lte/plugins/jqvmap/jquery.vmap.min.js"></script>
+    <script src="../../assets/admin_lte/plugins/jqvmap/maps/jquery.vmap.usa.js"></script>
+    <!-- jQuery Knob Chart -->
+    <script src="../../assets/admin_lte/plugins/jquery-knob/jquery.knob.min.js"></script>
+    <!-- daterangepicker -->
+    <script src="../../assets/admin_lte/plugins/moment/moment.min.js"></script>
+    <script src="../../assets/admin_lte/plugins/daterangepicker/daterangepicker.js"></script>
+    <!-- Tempusdominus Bootstrap 4 -->
+    <script src="../../assets/admin_lte/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js"></script>
+    <!-- Summernote -->
+    <script src="../../assets/admin_lte/plugins/summernote/summernote-bs4.min.js"></script>
+    <!-- overlayScrollbars -->
+    <script src="../../assets/admin_lte/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
+    <!-- AdminLTE App -->
+    <script src="../../assets/admin_lte/dist/js/adminlte.js"></script>
+    <!-- AdminLTE for demo purposes -->
+    <script src="../../assets/admin_lte/dist/js/demo.js"></script>
+    <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
+    <script src="../../assets/admin_lte/dist/js/pages/dashboard.js"></script>
+    <!-- DataTables  & Plugins -->
+    <script src="../../assets/admin_lte/plugins/datatables/jquery.dataTables.min.js"></script>
+    <script src="../../assets/admin_lte/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js"></script>
+    <script src="../../assets/admin_lte/plugins/datatables-responsive/js/dataTables.responsive.min.js"></script>
+    <script src="../../assets/admin_lte/plugins/datatables-responsive/js/responsive.bootstrap4.min.js"></script>
+    <script src="../../assets/admin_lte/plugins/datatables-buttons/js/dataTables.buttons.min.js"></script>
+    <script src="../../assets/admin_lte/plugins/datatables-buttons/js/buttons.bootstrap4.min.js"></script>
+    <script src="../../assets/admin_lte/plugins/jszip/jszip.min.js"></script>
+    <script src="../../assets/admin_lte/plugins/pdfmake/pdfmake.min.js"></script>
+    <script src="../../assets/admin_lte/plugins/pdfmake/vfs_fonts.js"></script>
+    <script src="../../assets/admin_lte/plugins/datatables-buttons/js/buttons.html5.min.js"></script>
+    <script src="../../assets/admin_lte/plugins/datatables-buttons/js/buttons.print.min.js"></script>
+    <script src="../../assets/admin_lte/plugins/datatables-buttons/js/buttons.colVis.min.js"></script>
+    <!-- Alert -->
+    <script src="../../assets/admin_lte/plugins/sweetalert2/sweetalert2.all.min.js"></script>
--- view/inventory/index.php
+++ view/inventory/index.php
@@ -5,15 +5,15 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>AdminLTE 3 | Dashboard</title>
-    <?php require("link_header.php") ?>
+    <?php require("../component/link_header.php") ?>
 </head>
 
 <body class="hold-transition sidebar-mini layout-fixed">
-    <?php require("manu.php") ?>
+    <?php require("../component/manu.php") ?>
     <!-- เนื้อหา -->
-    <h1>Hello</h1>
+
     <!-- เนื้อหา -->
-    <?php require("link_footer.php") ?>
+    <?php require("../component/link_footer.php") ?>
 </body>
 
 </html>
